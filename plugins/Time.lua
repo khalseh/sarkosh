967ebeb903e5446522d8f28dc0023975d852f907
khalseh <aryasmh@yahoo.com>
2017-06-09 18:41:28 +0430

    Update Time.lua

--- plugins/Time.lua
+++ plugins/Time.lua
@@ -1,4 +1,4 @@
---Begin Time.lua By @MahDiRoO
+--Begin Time.lua By @ketmaan @ForoshBotAntiSpam
 function run(msg, matches)
 local url , res = http.request('http://api.gpmod.ir/time/')
 if res ~= 200 then return "No connection" end
